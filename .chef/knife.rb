fd32c169e3835e509bb90d2eee310c35c04d18bb
wil.pannell <wil.pannell@gmail.com>
2013-10-15 12:19:22 -0700

    initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,13 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "wpannell"
+client_key               "#{current_dir}/wpannell.pem"
+validation_client_name   "ventana-validator"
+validation_key           "#{current_dir}/ventana-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/ventana"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
---
+++ cookbooks/learnchef-batch/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: learnchef-batch
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
