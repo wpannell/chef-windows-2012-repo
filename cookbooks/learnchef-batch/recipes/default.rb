a7854b697a84968c031e281c55cf85755bff4a50
wil.pannell <wil.pannell@gmail.com>
2013-10-15 20:01:32 -0700

    added windows batch cookbook

--- cookbooks/learnchef-batch/recipes/default.rb
+++ cookbooks/learnchef-batch/recipes/default.rb
@@ -1,8 +1,8 @@
-#
-# Cookbook Name:: learnchef-batch
-# Recipe:: default
-#
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+batch "Which platform?" do
+  code <<-EOH
+@ECHO OFF
+
+ECHO The platform = %OS% >> %SystemDrive%\plaform.txt
+
+  EOH
+end
