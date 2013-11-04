afcd3d9e80e6918a834df8fcc9de235a12ee22b0
wil.pannell <wil.pannell@gmail.com>
2013-11-04 14:27:16 -0800

    changed physical path for services app in iis

--- cookbooks/iis/recipes/vantage-app-create-services-sql-lite.rb
+++ cookbooks/iis/recipes/vantage-app-create-services-sql-lite.rb
@@ -9,7 +9,7 @@ iis_app "services" do
   app_name "vantage3"
   path "/services"
   application_pool "vantage-services-sql-lite"
-  physical_path "#{node['iis']['servicesroot']}"
+  physical_path "#{node['iis']['servicesroot']}\\HostIIS"
   action [:add]
 end
 
