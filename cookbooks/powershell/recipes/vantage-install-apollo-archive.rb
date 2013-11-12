a67dde4e3a4823a0d80eb82a01ef1021ec33da74
wil.pannell <wil.pannell@gmail.com>
2013-11-12 11:58:15 -0800

    trial to see it batch resource pipes subsequent lines into new command instance

--- cookbooks/powershell/recipes/vantage-install-apollo-archive.rb
+++ cookbooks/powershell/recipes/vantage-install-apollo-archive.rb
@@ -4,8 +4,10 @@ batch "install apollo service"  do
     cd "#{node['windows']['7ziproot']}
     7z x "#{node['windows']['temproot']}\\apache-apollo-1.6-windows-distro.zip" -o"#{ENV['SYSTEMDRIVE']}"
     cd \\apache-apollo-1.6\\bin
+    apollo.cmd
     create ApolloService
     ApolloService\\bin\\install
     ApolloService\\bin\\start
+    exit
   EOH
 end
--- cookbooks/powershell/recipes/vantage-install-jdk-archive.rb
+++ cookbooks/powershell/recipes/vantage-install-jdk-archive.rb
@@ -1,7 +1,11 @@
 batch "install jdk"  do
   code <<-EOH
-  c:
-  cd "#{node['windows']['temproot']}""
-  cmd /c jdk-6u18-windows-x64.exe /s
+    c:
+    cd "#{node['windows']['temproot']}"
+    cmd /c jdk-6u18-windows-x64.exe /s
   EOH
+end
+
+env "JAVA_HOME" do
+  value C:\\Program Files\\Java\\jre6
 end
