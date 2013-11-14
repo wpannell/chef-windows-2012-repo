9c18001cb3c76344d3fb561e5ad2ac943fd9eb01
wil.pannell <wil.pannell@gmail.com>
2013-11-14 15:55:02 -0800

    reconfigured install apollo recipe

--- cookbooks/powershell/recipes/vantage-install-apollo-archive.rb
+++ cookbooks/powershell/recipes/vantage-install-apollo-archive.rb
@@ -1,13 +1,23 @@
+env "JAVA_HOME" do
+  value "C:\\Program Files\\Java\\jre6"
+  action :create
+end
+
+batch "expand apollo archive"  do
+  code <<-EOH
+    c:
+    cd "#{node['windows']['7ziproot']}"
+    7z x "#{node['windows']['temproot']}\\apache-apollo-1.6-windows-distro.zip" -o"#{ENV['SYSTEMDRIVE']}"
+  EOH
+end
+
 batch "install apollo service"  do
   code <<-EOH
     c:
-    cd "#{node['windows']['7ziproot']}
-    7z x "#{node['windows']['temproot']}\\apache-apollo-1.6-windows-distro.zip" -o"#{ENV['SYSTEMDRIVE']}"
-    cd \\apache-apollo-1.6\\bin
-    apollo.cmd
-    create ApolloService
-    ApolloService\\bin\\install
-    ApolloService\\bin\\start
-    exit
+    cd "\\"
+    \\apache-apollo-1.6\\bin\\apollo "create apollo-message-broker"
+    \\apollo-message-broker\\bin\\apollo-broker-service "install"
+    \\apollo-message-broker\\bin\\apollo-broker-service "start"
   EOH
 end
+
