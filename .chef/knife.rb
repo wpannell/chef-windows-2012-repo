95a8ab8616d2109621121da3d5ebabf2b5eed82e
wil.pannell <wil.pannell@gmail.com>
2013-11-22 13:35:13 -0800

    reset validation keys

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,5 +1,3 @@
-# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
-
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
