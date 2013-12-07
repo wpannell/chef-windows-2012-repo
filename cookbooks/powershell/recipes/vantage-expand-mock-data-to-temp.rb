ffbc66bf08475465e0a8fbf876ea7cadb1536e17
wil.pannell <wil.pannell@gmail.com>
2013-12-07 13:25:54 -0800

    updated expand mock data archive reciper

--- cookbooks/powershell/recipes/vantage-expand-mock-data-to-temp.rb
+++ cookbooks/powershell/recipes/vantage-expand-mock-data-to-temp.rb
@@ -2,7 +2,7 @@ batch "expand mock data to temporary folder"  do
   code <<-EOH
     c:
     cd #{node['windows']['7ziproot']}
-    7z x #{node['windows']['temproot']}\\vantage3.db.zip
+    7z x #{node['windows']['temproot']}\\vantage3.db.zip -o"#{node['windows']['temproot']}"
   EOH
 end
 
