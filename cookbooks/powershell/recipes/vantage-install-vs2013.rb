07dab77012f9eb8f11bd1f83f5bd13bceb588e5b
wil.pannell <wil.pannell@gmail.com>
2013-12-23 12:16:30 -0800

    trials attempting to get visual studio install to work

--- cookbooks/powershell/recipes/vantage-install-vs2013.rb
+++ cookbooks/powershell/recipes/vantage-install-vs2013.rb
@@ -2,6 +2,6 @@ batch "install visual studio 2013"  do
   code <<-EOH
     c:
     cd "\\"
-    cmd /c f:\\vs_professional.exe /adminfile AdminDeployment.xml /passive /norestart
+    f:\\vs_professional.exe /adminfile AdminDeployment.xml /passive /norestart
   EOH
 end
