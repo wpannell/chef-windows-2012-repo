5636583adff8f52abff0cf8f517c67d2bda45c6b
wil.pannell <wil.pannell@gmail.com>
2013-12-29 19:21:21 -0800

    now supports node version manager on windows

---
+++ cookbooks/powershell/recipes/vantage-copy-nodedist-archive.rb
@@ -0,0 +1,3 @@
+cookbook_file "#{node['windows']['temproot']}\\marcelklehr-nodist-v0.4.2-0-gce884a0.zip"  do
+  source "marcelklehr-nodist-v0.4.2-0-gce884a0.zip"
+end
---
+++ cookbooks/powershell/recipes/vantage-install-nodist.rb
@@ -0,0 +1,13 @@
+batch "install nodedist"  do
+  code <<-EOH
+    c:
+    cd #{node['windows']['7ziproot']}
+    7z x #{node['windows']['temproot']}\\marcelklehr-nodist-v0.4.2-0-gce884a0.zip -o#{ENV['SYSTEMDRIVE']}\\nodedist
+  EOH
+end
+
+windows_path '#{ENV['SYSTEMDRIVE']}\\nodedist\\bin' do
+  action :add
+end
+
+
