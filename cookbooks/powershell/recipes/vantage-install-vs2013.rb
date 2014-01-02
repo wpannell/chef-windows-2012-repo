d9ec876a1fc241ab7643f5ffcd51b4e9a2565a00
wil.pannell <wil.pannell@gmail.com>
2014-01-01 20:09:37 -0800

    hacks to get visual studio installed

--- cookbooks/powershell/recipes/vantage-install-vs2013.rb
+++ cookbooks/powershell/recipes/vantage-install-vs2013.rb
@@ -2,6 +2,6 @@ batch "install visual studio 2013"  do
   code <<-EOH
     c:
     cd "\\"
-    e:\\vs_professional.exe /AdminFile c:\\temp\\AdminDeployment.xml /passive /quiet /norestart /log c:\\vs2013.log
+    e:\\vs_ultimate.exe /AdminFile c:\\temp\\AdminDeployment.xml /passive /quiet /norestart /log c:\\vs2013.log
   EOH
 end
