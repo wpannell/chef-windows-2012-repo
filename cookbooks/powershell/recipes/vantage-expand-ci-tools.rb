5bbc2768ae01e8ea1ef36d8978ccdd913357e20a
wil.pannell <wil.pannell@gmail.com>
2014-01-02 20:20:25 -0800

    reverted to install vs2013 and node on startup

--- cookbooks/powershell/recipes/vantage-expand-ci-tools.rb
+++ cookbooks/powershell/recipes/vantage-expand-ci-tools.rb
@@ -3,12 +3,13 @@ batch "expand ci-tools"  do
     c:
     cd #{node['windows']['7ziproot']}
     7z x #{node['windows']['temproot']}\\ci-tools.zip -o#{node['windows']['temproot']}
+    move #{node['windows']['temproot']}\\install-node-tools.bat "C:\\Users\\Administrator\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+    move #{node['windows']['temproot']}\\install-vs2013-tools.bat "C:\\Users\\Administrator\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
   EOH
+  notifies :request, 'windows_reboot[60]'
 end
 
-batch "install node" do
-  code <<-EOH
-    set PATH = "C:\\nodist\\bin;%PATH%"
-    c:\\temp\\install-node-tools
-  EOH
+windows_reboot 60 do
+  reason 'CI-TOOLS installed on reboot'
+  action :nothing
 end
