5bd6e193cb3aac542f9c330151e4dd0f1f97a2ba
wil.pannell <wil.pannell@gmail.com>
2014-01-03 09:48:03 -0800

    now skips over pre-existing files when extracting jenkins plugins

--- cookbooks/powershell/recipes/vantage-install-jenkins-config.rb
+++ cookbooks/powershell/recipes/vantage-install-jenkins-config.rb
@@ -2,8 +2,8 @@ batch "expand jenkins config archive"  do
   code <<-EOH
     c:
     cd #{node['windows']['7ziproot']}
-    7z x #{node['windows']['temproot']}\\jenkins.config.zip -o"#{ENV['SYSTEMDRIVE']}\\Program Files (x86)\\Jenkins" -y
-    7z x #{node['windows']['temproot']}\\plugins1.zip -o"#{ENV['SYSTEMDRIVE']}\\Program Files (x86)\\Jenkins\\plugins" -y
-    7z x #{node['windows']['temproot']}\\plugins2.zip -o"#{ENV['SYSTEMDRIVE']}\\Program Files (x86)\\Jenkins\\plugins" -y
+    7z x #{node['windows']['temproot']}\\jenkins.config.zip -o"#{ENV['SYSTEMDRIVE']}\\Program Files (x86)\\Jenkins" -aos
+    7z x #{node['windows']['temproot']}\\plugins1.zip -o"#{ENV['SYSTEMDRIVE']}\\Program Files (x86)\\Jenkins\\plugins" -aos
+    7z x #{node['windows']['temproot']}\\plugins2.zip -o"#{ENV['SYSTEMDRIVE']}\\Program Files (x86)\\Jenkins\\plugins" -aos
   EOH
 end
