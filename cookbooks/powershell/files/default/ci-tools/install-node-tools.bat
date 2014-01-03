b07eb9d3ed935d17c03d0e1c1045d34d00650259
wil.pannell <wil.pannell@gmail.com>
2014-01-02 19:05:37 -0800

    trial to install node prior to reboot

---
+++ cookbooks/powershell/files/default/ci-tools/install-node-tools.bat
@@ -0,0 +1,9 @@
+cmd /c C:\temp\configure-nodist.bat
+
+
+
+cmd /c C:\temp\install-node.bat
+
+
+
+cmd /c C:\temp\set-node-default.bat
---
+++ cookbooks/powershell/files/default/ci-tools/install-vs2013-tools.bat
@@ -0,0 +1,2 @@
+e:\vs_ultimate.exe /passive /quiet /norestart /noweb /full /Log c:\vs2013.log
+
